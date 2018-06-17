7b6832c1d7170a7c6c9a5606d593cfff7927c77d
Simon Holmes <srsholmes@gmail.com>
2018-06-17 22:04:08 +0100

    Initial Commit

---
+++ snapshot-env-menu-bar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  snapshot-env-menu-bar
+//
+//  Created by Simon Holmes on 17/06/2018.
+//  Copyright © 2018 Simon Holmes. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ snapshot-env-menu-bar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  snapshot-env-menu-bar
+//
+//  Created by Simon Holmes on 17/06/2018.
+//  Copyright © 2018 Simon Holmes. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
